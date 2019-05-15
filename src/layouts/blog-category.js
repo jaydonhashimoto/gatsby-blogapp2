1137446544036732322dd1ea5087fa01e69a4cf9
jaydonhashimoto <jaydonhashi@gmail.com>
2019-05-14 23:35:23 -0700

    added author and category layouts

---
+++ src/layouts/blog-category.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Row, Cell } from "griding"
+
+import { Container } from "../components/grid"
+import * as S from "../components/styles.css"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Pagination from "../components/pagination"
+import renderList from "../components/renderList"
+
+const BlogCategory = ({ data, pageContext }) => {
+    const { allMarkdownRemark } = data
+    const { currentPage, numPages, category, allCategories } = pageContext
+
+    return (
+        <Layout>
+            <Seo title={category} />
+
+            <Container>
+                <Row>
+                    <Cell xs={12}>
+                        <S.HeaderSectionTitle>Categories:</S.HeaderSectionTitle>
+                        <S.HeaderSectionList>
+                            {allCategories.map(cat => (
+                                <S.HeaderSectionLink to={`/blog/category/${cat}`}>
+                                    {cat}
+                                </S.HeaderSectionLink>
+                            ))}
+                        </S.HeaderSectionList>
+                    </Cell>
+
+                    {allMarkdownRemark.edges.map(renderList)}
+                </Row>
+
+                <Pagination
+                    currentPage={currentPage}
+                    numPages={numPages}
+                    contextPage={`category/${category}`}
+                />
+            </Container>
+        </Layout>
+    )
+}
+
+export default BlogCategory
+
+export const query = graphql`
+  query blogPostsListByCategory($category: String, $skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { category: { in: [$category] } } }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            author
+            category
+            image {
+              childImageSharp {
+                fluid {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
